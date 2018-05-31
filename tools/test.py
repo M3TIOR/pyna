30645b6698653201c99b034fb86084e57fd7e7d5
M3TIOR <jtimmerman32@gmail.com>
2018-05-31 04:58:06 -0700

    added tests for DNAEncoding class to test suite

--- tools/test.py
+++ tools/test.py
@@ -3,6 +3,7 @@
 # import unittest and test modules
 import unittest
 import tests.dna2bytes as dna2bytes
+import tests.dnaencoding as dnaencoding
 
 
 if __name__ == "__main__":
@@ -14,6 +15,11 @@ if __name__ == "__main__":
 	suite.addTest( dna2bytes.DNA2Bytes('test_head_static') )
 	suite.addTest( dna2bytes.DNA2Bytes('test_flipTail_static') )
 	suite.addTest( dna2bytes.DNA2Bytes('test_flipHead_static') )
+
+	suite.addTest( dnaencoding.DNAEncoding('test_new_instance') )
+	suite.addTest( dnaencoding.DNAEncoding('test_rebinding') )
+	suite.addTest( dnaencoding.DNAEncoding('test_wrong_keycount_rebinding') )
+	suite.addTest( dnaencoding.DNAEncoding('test_redundant_index_rebinding') )
 
 	# Run all test modules!
 	runner.run(suite)
