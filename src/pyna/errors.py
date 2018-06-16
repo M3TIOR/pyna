b08afc7f77e9fc0073a8bd8e85c5fc5f5b89f7e8
M3TIOR <jtimmerman32@gmail.com>
2018-06-15 20:38:33 -0700

    removed some deprecated errors from errors.py

--- src/pyna/errors.py
+++ src/pyna/errors.py
@@ -19,35 +19,8 @@ class NucleotideError(Error):
 	""" Errors that arise because of conflicts with Nucleotides """
 	pass
 
-class BindingError(NucleotideError):
-	""" Raised when attempting to pair a nucleotide that already has a sibling
-		or when a transient tries to pair itself with another transient"""
-	pass
-
 class BioEncodingError(Error):
 	""" Errors raised from problems with BioEncodings """
-	pass
-
-class ExecutableError(BioEncodingError):
-	""" An error that is raised when attempting to encode with an executable BioEncoding"""
-	# XXX:
-	#	DNA cannot be encoded. The mollecule's format simply can't be
-	#	interpreted as quadernary; when it's structure, via the existence of
-	#	matching base pairs opposite each nucleotide, imply that
-	#	that each nucleotide pair instead represents a binary signal, on or off.
-	#	Since otherwise, the resulting encoding would be identical to that of its
-	#	RNA counterpart. Which wouldn't make any sense. Especially considering that
-	#	RNA is only used for transcription and storage with in cells
-	#
-	#	I could have the functions of DNA and RNA backwards, but for now, this is
-	#	the hypothesis I'm running with.
-	#
-	#	So when DNA is being executed / read, having to read both of it's sides
-	#	individually would be inefficient. This isn't to say that I think a
-	#	rudimentary organism is smart enough to optimize itself, but perhaps to say
-	#	that adding redundancy to the most simple form of life seems like overkill.
-	#
-	#	Hence the addition of the executable vs storage encoding types
 	pass
 
 class TranslationError(BioEncodingError):
